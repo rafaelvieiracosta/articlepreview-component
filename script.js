032ee69e9823c901bbd7e4bcbdb2209a04c6d14e
Rafael <rvc.vieira@outlook.com>
2022-03-29 16:35:56 -0300

    fix: mobile styles

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ const btn = document.querySelector(".button");
 const div = document.querySelector(".share");
 
 btn.addEventListener("click", () => {
-  if (div.style.display === "flex") {
-    div.style.display = "none";
-  } else {
-    div.style.display = "flex";
-  }
-  
-  setTimeout(() => {
     div.classList.toggle("ativo");
     btn.classList.toggle("ativo");
-  }, 1);
 });
